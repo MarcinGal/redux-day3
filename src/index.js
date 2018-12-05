104fbe0e2db06ef1d9e6ed2975b72382e17ac735
marcingal <marcin921125@gmail.com>
2018-12-05 10:17:17 +0100

    initializing counter

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import App from './App';
 
 import { store } from './store'
 
+window.__REDUX_DEVTOOLS_EXTENSION__ &&
+window.__REDUX_DEVTOOLS_EXTENSION__()
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/state/counter.js
@@ -0,0 +1,14 @@
+const INC = 'counter/INC'
+const DEC = 'counter/DEC'
+
+const INITIAL_STATE = {
+    number: 0
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type){
+
+        default:
+        return state
+    }
+}
